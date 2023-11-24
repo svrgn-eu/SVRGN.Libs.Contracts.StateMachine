794b289e844cf01d38a8f0fc9ebded09f89fafab
efilnukefesin <efil.threat@googlemail.com>
2023-11-24 22:58:46 +0100

    added comments and documentation

---
+++ README.md
@@ -0,0 +1,28 @@
+[TOC]
+
+# Purpose
+
+The purpose of this library is to provide interfaces for a flexible and robust state machine. 
+
+One of the main goals is to not only build a state machine being able to switch states, but also to handle transitions.
+
+You are able to add actions to the following events:
+
+- State
+  - Enter
+  - Update
+  - Exit
+- Transition
+  - Enter
+  - in Between
+  - Exit
+
+# Why?
+
+Why would you even want to use that high number of Actions here? It is because the pipeline is flexible enough to support scenarios where you can do specific actions for specific state transitions while still being able to have common state actions.
+
+
+
+# Examples
+
+tbd
--- src/SVRGN.Libs.Contracts.StateMachine/IStateMachine.cs
+++ src/SVRGN.Libs.Contracts.StateMachine/IStateMachine.cs
@@ -8,24 +8,82 @@ namespace SVRGN.Libs.Contracts.StateMachine
     public interface IStateMachine : IBaseObject, IUpdate, IClear, IToMermaid, IFromMermaid
     {
         #region Properties
+
+        #region StateCount: the total number of states
+        /// <summary>
+        /// the total number of states
+        /// </summary>
         int StateCount { get; }
+        #endregion StateCount
+
+        #region TransitionCount: the total number of transitions
+        /// <summary>
+        /// the total number of transitions
+        /// </summary>
         int TransitionCount { get; }
+        #endregion TransitionCount
 
         #endregion Properties
 
         #region Methods
 
+        #region SetUpdateInterval
         void SetUpdateInterval(TimeSpan NewInterval);
+        #endregion SetUpdateInterval
+
+        #region AddState: Adds a new state
+        /// <summary>
+        /// Adds a new state
+        /// </summary>
+        /// <param name="StateName">the name of the new state</param>
+        /// <returns>true if successful, false if the state already exists</returns>
         bool AddState(string StateName);
+        #endregion AddState
+
+        #region StartWith: set the start state
+        /// <summary>
+        /// set the start state
+        /// </summary>
+        /// <param name="StateName">the name of the starting state, if the state name does not exist, the first transition will fail</param>
         void StartWith(string StateName);
+        #endregion StartWith
+
+        #region AddTransition
         bool AddTransition(string TransitionName, string FromStateName, string ToStateName);
+        #endregion AddTransition
+
+        #region AddTransitionEnterAction
         bool AddTransitionEnterAction(string TransitionName, Action ActionToTake);
+        #endregion AddTransitionEnterAction
+
+        #region AddTransitionAction
         bool AddTransitionAction(string TransitionName, Action ActionToTake);
+        #endregion AddTransitionAction
+
+        #region AddTransitionExitAction
         bool AddTransitionExitAction(string TransitionName, Action ActionToTake);
+        #endregion AddTransitionExitAction
+
+        #region AddStateEnterAction
         bool AddStateEnterAction(string StateName, Action ActionToTake);
+        #endregion AddStateEnterAction
+
+        #region AddStateUpdateAction
         bool AddStateUpdateAction(string StateName, Action ActionToTake);
+        #endregion AddStateUpdateAction
+
+        #region AddStateExitAction
         bool AddStateExitAction(string StateName, Action ActionToTake);
+        #endregion AddStateExitAction
+
+        #region Transition: performs a transition by it's name
+        /// <summary>
+        /// performs a transition by it's name
+        /// </summary>
+        /// <param name="TransitionName">the name of the transition</param>
+        /// <returns>true, if the transition has been performed successfully, false if the transition has not been found</returns>
         bool Transition(string TransitionName);
+        #endregion Transition
 
         #region TransitionTo: Transitions into a Target state, the state machine will check, if possible and then do the transition
         /// <summary>
@@ -36,7 +94,13 @@ namespace SVRGN.Libs.Contracts.StateMachine
         bool TransitionTo(string TargetStateName);
         #endregion TransitionTo
 
+        #region GetCurrentStateName: returns the name of the currently active state
+        /// <summary>
+        /// returns the name of the currently active state
+        /// </summary>
+        /// <returns>the name of the currently active state</returns>
         string GetCurrentStateName();
+        #endregion GetCurrentStateName
 
         #endregion Methods
     }
